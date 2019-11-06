b3e76accdfc34f45d432508192acb9132edc8a85
Sokratis Vogiatzakis <tp4412@edu.teicrete.gr>
2019-11-06 19:56:16 +0200

    New ship materials, scripted some movement + audio.

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    AudioSource audio;
+    public int force = 10;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * force);
+            if (!audio.isPlaying) { audio.Play(); }
+        }
+        else { audio.Stop(); }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+
+    }
+}
