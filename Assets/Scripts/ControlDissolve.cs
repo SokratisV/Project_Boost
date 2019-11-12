7d052e8350fcdd7f6e01990b967c208b08db40af
Sokratis Vogiatzakis <sokvogiatzakis@gmail.com>
2019-11-12 21:08:49 +0200

    Created new dissolve shader.

---
+++ Assets/Scripts/ControlDissolve.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ControlDissolve : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            foreach (var item in transform.GetComponentsInChildren<Dissolve>())
+            {
+                item.StartDissolving();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Dissolve.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour
+{
+    [SerializeField] float interval = default;
+    [SerializeField] float incrementAmount = default;
+    WaitForSeconds _interval = default;
+    Material material;
+
+    private void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        _interval = new WaitForSeconds(interval);
+    }
+
+    public void StartDissolving()
+    {
+        StartCoroutine(_StartDissolving());
+    }
+
+    private IEnumerator _StartDissolving()
+    {
+        float incrementValue = 0;
+        while (true)
+        {
+            material.SetFloat("_Effect", incrementValue += incrementAmount);
+            if (incrementValue >= 1) break;
+            yield return _interval;
+        }
+    }
+}
