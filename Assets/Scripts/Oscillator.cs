3b8f01cbfefffef592a8a586dc8c1e1cfe9ea31a
Sokratis Vogiatzakis <sokvogiatzakis@gmail.com>
2019-12-18 02:03:33 +0200

    Changed Oscillator script, further improved scenes.

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -1,14 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
-    [Range(0, 1)] [SerializeField] float movementFactor;
+    //Move Old
+    // [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    // [Range(0, 1)] [SerializeField] float movementFactor;
+    // [SerializeField] float period = 2f;
+    [SerializeField] float rotationSpeed = 5f, translationSpeed = 1f;
+    [SerializeField]
+    bool shouldRotate = true, shouldMove = true,
+        rotateLocal = false, moveLocal = false,
+        rotateX = false, rotateY = true, rotateZ = false,
+        translateX = false, translateY = true, translateZ = false;
+    [SerializeField] AnimationCurve curve;
 
     Vector3 startingPos;
-    [SerializeField] float period = 2f;
     const float tau = Mathf.PI * 2;
+    float deltaTime;
 
     private void Start()
     {
@@ -17,12 +27,68 @@ public class Oscillator : MonoBehaviour
 
     private void Update()
     {
-        if (period <= Mathf.Epsilon) { return; }
-        float cycle = Time.time / period;
-        float rawSinWave = Mathf.Sin(cycle * tau);
+        deltaTime = Time.deltaTime;
+        Move();
+        Rotate();
+    }
 
-        movementFactor = rawSinWave / 2f + .5f;
-        Vector3 offset = movementFactor * movementVector;
-        transform.position = startingPos + offset;
+    private void Move()
+    {
+        if (!shouldMove) return;
+        float v = curve.Evaluate(Time.time);
+        Vector3 translation = GetTranslationVector(v) * translationSpeed;
+        if (moveLocal)
+        {
+            transform.Translate(translation, Space.Self);
+        }
+        else
+        {
+            transform.Translate(translation, Space.World);
+        }
+    }
+
+    private Vector3 GetTranslationVector(float v)
+    {
+        return new Vector3(v * deltaTime * ActivateAxis(translateX),
+            v * deltaTime * ActivateAxis(translateY),
+            v * deltaTime * ActivateAxis(translateZ));
+    }
+
+    private void Rotate()
+    {
+        if (!shouldRotate) return;
+        Vector3 rotationVector = GetRotationVector() * rotationSpeed;
+        if (rotateLocal)
+        {
+            transform.Rotate(rotationVector, Space.Self);
+        }
+        else
+        {
+            transform.Rotate(rotationVector, Space.World);
+        }
+    }
+
+    private Vector3 GetRotationVector()
+    {
+        return new Vector3(deltaTime * ActivateAxis(rotateX),
+            deltaTime * ActivateAxis(rotateY),
+            deltaTime * ActivateAxis(rotateZ));
+    }
+
+    private float ActivateAxis(bool condition)
+    {
+        if (condition) { return 1; }
+        return 0;
+    }
+
+    private void OldMove()
+    {
+        // if (period <= Mathf.Epsilon || !shouldMove) { return; }
+        // float cycle = Time.time / period;
+        // float rawSinWave = Mathf.Sin(cycle * tau);
+
+        // movementFactor = rawSinWave / 2f + .5f;
+        // Vector3 offset = movementFactor * movementVector;
+        // transform.position = startingPos + offset;
     }
 }
